d05b243c90c5ba66abf92ce03db6b1554e2594e3
snegaa <26.rain@mail.ru>
2017-04-09 14:13:59 +0300

    доработаь цикл, добавлять ост фомы

--- all/models.py
+++ all/models.py
@@ -2,35 +2,49 @@ from django.db import models
 
 # Create your models here.
 
+
 class People(models.Model):
     number_pasport = models.IntegerField(verbose_name = 'Номер паспорта')
     seria_pasport = models.IntegerField(verbose_name='Серия паспорта')
     familia = models.TextField(max_length = 20, verbose_name='Фамилия')
     imya = models.TextField(max_length = 20, verbose_name='Имя')
-    otch = models.TextField(max_length = 20, verbose_name = 'Отчество')
+    otchestvo = models.TextField(max_length = 20, verbose_name = 'Отчество')
     parol = models.IntegerField(verbose_name='Пароль')
 
+    class Meta:
+        unique_together = ('number_pasport', 'seria_pasport')
+
+    def __str__(self):
+        return '{0} {1}'.format(self.imya, self.familia)
 
 
 class Dolzhnost(models.Model):
-    speshion = models.CharField(max_length=50, verbose_name='Должность')
+    position = models.CharField(max_length=50, verbose_name='Должность')
+
+    def __str__(self):
+        return self.position
 
 
 class Vrach(models.Model):
-    p = models.ForeignKey(People)
-    d = models.ForeignKey(Dolzhnost)
+    FK_People = models.ForeignKey(People)
+    FK_position = models.ForeignKey(Dolzhnost)
+    price_ferst = models.IntegerField(default=0, verbose_name='Цена первичного приема')
+    price_second = models.IntegerField(default=0, verbose_name='Цена повторного приема')
+
+    def __str__(self):
+        return '{0}, {1}'.format(str(self.FK_People), str(self.FK_position))
 
 
 class Bolnoy(models.Model):
-    p = models.ForeignKey(People)
+    FK_People = models.ForeignKey(People)
     e_mail = models.EmailField()
 
 
 
 class History(models.Model):
     date = models.DateField()
-    zhaloby = models.TextField(max_length=500, verbose_name='Жалобы')
-    diagnoz = models.TextField(max_length=500, verbose_name='Диагноз')
+    complaint = models.TextField(max_length=500, verbose_name='Жалобы')
+    diagnosis = models.TextField(max_length=500, verbose_name='Диагноз')
 
 
 class Smena(models.Model):
--- all/views.py
+++ all/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Vrach
 # Create your views here.
 
 
 def hell(request):
     print(request.POST)
     print(request.GET)
+    all_vrach = Vrach.objects.all()
 
-    return HttpResponse(render(request, 'main.html',  {'flag':True}))
+    return HttpResponse(render(request, 'main.html',  {'flag':True, 'all_vrach': all_vrach}))
